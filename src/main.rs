3c8b7cd52c8f9e69310daf489845b1e5e4a190ca
Samuel Tri <samueltri5590@gmail.com>
2024-07-21 00:38:27 +1000

    New branch
    Created new branch to test and work on esp now communication

--- src/main.rs
+++ src/main.rs
@@ -1,147 +1,112 @@
-//! embassy hello world
+//! ESP-NOW Example
 //!
-//! This is an example of running the embassy executor with multiple tasks
-//! concurrently.
+//! Broadcasts, receives and sends messages via esp-now
 
-//% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
-//% FEATURES: embassy esp-hal-embassy/integrated-timers
+//% FEATURES: esp-wifi esp-wifi/wifi-default esp-wifi/wifi esp-wifi/utils esp-wifi/esp-now
+//% CHIPS: esp32 esp32s2 esp32s3 esp32c2 esp32c3 esp32c6
 
 #![no_std]
 #![no_main]
 
-use core::{cell::RefCell, sync::atomic::Ordering};
-
-use atomic_float::AtomicF32;
-use critical_section::Mutex;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
-    delay::Delay,
-    gpio::{self, Event, Input, Io, Level, Output, Pull},
-    macros::ram,
     peripherals::Peripherals,
     prelude::*,
+    rng::Rng,
     system::SystemControl,
+    timer::PeriodicTimer,
 };
-use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
-use heapless::spsc::Queue;
+use esp_println::println;
+use esp_wifi::{
+    current_millis,
+    esp_now::{PeerInfo, BROADCAST_ADDRESS},
+    initialize,
+    EspWifiInitFor,
+};
 
-mod esda_interface;
+use byteorder::{Byteorder, LittleEndian}; // Import Byteorder
 
-// 18, 19, 21
-static ENCODER_LEFT_A: Mutex<RefCell<Option<Input<gpio::Gpio18>>>> = Mutex::new(RefCell::new(None));
-static ENCODER_LEFT_D: Mutex<RefCell<Option<Input<gpio::Gpio19>>>> = Mutex::new(RefCell::new(None));
-static ENCODER_LEFT_TICKS: AtomicF32 = AtomicF32::new(0.0);
-
-// 34, 33, 32
-static ENCODER_RIGHT_A: Mutex<RefCell<Option<Input<gpio::Gpio34>>>> = Mutex::new(RefCell::new(None));
-static ENCODER_RIGHT_D: Mutex<RefCell<Option<Input<gpio::Gpio33>>>> = Mutex::new(RefCell::new(None));
-static ENCODER_RIGHT_TICKS: AtomicF32 = AtomicF32::new(0.0);
-
-#[embassy_executor::task]
-async fn run() {
-    loop {
-        esp_println::println!("Hello world from embassy using esp-hal-async!");
-        Timer::after(Duration::from_millis(1_000)).await;
-    }
-}
-
-#[main]
-async fn main(spawner: Spawner) {
+#[entry]
+fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
-    esp_println::println!("Init!");
     let peripherals = Peripherals::take();
+
     let system = SystemControl::new(peripherals.SYSTEM);
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+    let clocks = ClockControl::max(system.clock_control).freeze();
 
-    let mut io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    io.set_interrupt_handler(handler);
-
-    let encoder_left_a = io.pins.gpio18;
-    let encoder_left_d = io.pins.gpio19;
-    let encoder_right_a = io.pins.gpio34;
-    let encoder_right_d = io.pins.gpio33;
-    let mut encoder_left_a = Input::new(encoder_left_a, Pull::Down);
-    let mut encoder_left_d = Input::new(encoder_left_d, Pull::Down);
-    let mut encoder_right_a = Input::new(encoder_right_a, Pull::Down);
-    let mut encoder_right_d = Input::new(encoder_right_d, Pull::Down);
-
-    critical_section::with(|cs| {
-        encoder_left_a.listen(Event::RisingEdge);
-        ENCODER_LEFT_A.borrow_ref_mut(cs).replace(encoder_left_a);
-    });
-    critical_section::with(|cs| {
-        encoder_right_a.listen(Event::RisingEdge);
-        ENCODER_RIGHT_A.borrow_ref_mut(cs).replace(encoder_right_a);
-    });
-    critical_section::with(|cs| {
-        ENCODER_LEFT_D.borrow_ref_mut(cs).replace(encoder_left_d);
-    });
-    critical_section::with(|cs| {
-        ENCODER_RIGHT_D.borrow_ref_mut(cs).replace(encoder_right_d);
-    });
-
-    esp_println::println!("Fully Initialised!");
-
-    // let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks, None);
-    // let timer0 = OneShotTimer::new(timg0.timer0.into());
-    // let timers = [timer0];
-    // let timers = mk_static!([OneShotTimer<ErasedTimer>; 1], timers);
-    // esp_hal_embassy::init(&clocks, timers);
-
-    // spawner.spawn(run()).ok();
-
-    // loop {
-    //     esp_println::println!("Bing!");
-    //     Timer::after(Duration::from_millis(5_000)).await;
-    // }
-
-
-}
-
-#[handler]
-#[ram]
-fn handler() {
-    esp_println::println!(
-        "GPIO Interrupt with priority {}",
-        esp_hal::xtensa_lx::interrupt::get_level()
+    let timer = PeriodicTimer::new(
+        esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0, &clocks, None)
+            .timer0
+            .into(),
     );
-    
-    critical_section::with(|cs| {
-        // Check that the left encoder caused an interrupt (since interrupt is rising edge, does not debounce)
-        if ENCODER_LEFT_A.borrow_ref_mut(cs).as_mut().unwrap().is_high() {
-            ENCODER_LEFT_TICKS.fetch_update(Ordering::SeqCst, Ordering::SeqCst, |current_ticks| { 
-                Some(match ENCODER_LEFT_D.borrow_ref(cs).as_ref().unwrap().get_level() {
-                    Level::Low => current_ticks+1.0,
-                    Level::High => current_ticks-1.0,
-                })
-            }).unwrap();
-            // Reset the left encoder's interrupt status
-            ENCODER_LEFT_A
-                .borrow_ref_mut(cs)
-                .as_mut()
-                .unwrap()
-                .clear_interrupt()
-        }
-    });
 
-    critical_section::with(|cs| {
-        // Check that the left encoder caused an interrupt (since interrupt is rising edge, does not debounce)
-        if ENCODER_RIGHT_A.borrow_ref_mut(cs).as_mut().unwrap().is_high() {
-            ENCODER_RIGHT_TICKS.fetch_update(Ordering::SeqCst, Ordering::SeqCst, |current_ticks| { 
-                Some(match ENCODER_RIGHT_D.borrow_ref(cs).as_ref().unwrap().get_level() {
-                    Level::Low => current_ticks+1.0,
-                    Level::High => current_ticks-1.0,
-                })
-            }).unwrap();
-            // Reset the right encoder's interrupt status
-            ENCODER_RIGHT_A
-                .borrow_ref_mut(cs)
-                .as_mut()
-                .unwrap()
-                .clear_interrupt()
+    let init = initialize(
+        EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+        &clocks,
+    )
+    .unwrap();
+
+    let wifi = peripherals.WIFI;
+    let mut esp_now = esp_wifi::esp_now::EspNow::new(&init, wifi).unwrap();
+
+    println!("esp-now version {}", esp_now.get_version().unwrap());
+
+    let mut next_send_time = current_millis() + 5 * 1000;
+    let mut cntr: u32 = 0; // Initializes an integer counter 
+
+    loop {
+        let r = esp_now.receive();
+        if let Some(r) = r {
+
+            println!("Received message from {:?}", r.info.src_address);
+            let data = r.data;
+            if data.len() >= 4 {
+                let received_number = LittleEndian::read_u32(&data[0..4]);
+                println!("Received number: {}", received_number);
+            } else {
+                println!("Received data is too short to decode as u32.");
+            }
+            
+            // println!("Received data as {:?}", data);
+
+
+            //println!("Received {:?}", r);
+
+            if r.info.dst_address == BROADCAST_ADDRESS {
+                if !esp_now.peer_exists(&r.info.src_address) {
+                    esp_now
+                        .add_peer(PeerInfo {
+                            peer_address: r.info.src_address,
+                            lmk: None,
+                            channel: None,
+                            encrypt: false,
+                        })
+                        .unwrap();
+                }
+                let status = esp_now
+                    .send(&r.info.src_address, b"Hello Peer")
+                    .unwrap()
+                    .wait();
+                println!("Send hello to peer status: {:?}", status);
+            }
         }
-    });
+
+        if current_millis() >= next_send_time {
+            next_send_time = current_millis() + 5 * 1000;
+            println!("Send");
+            // Increment counter and send
+            let mut buffer = [0; 4];
+            LittleEndian::write_u32(&mut buffer, cntr);
+            let status = esp_now
+                .send(&BROADCAST_ADDRESS, &buffer)
+                .unwrap()
+                .wait();
+            println!("Send broadcast status: {:?}", status)
+        }
+    }
 }
