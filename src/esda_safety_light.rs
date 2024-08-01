8ba80096bbae5d73ab795ad420d4db517d26ae8e
BMCG0011 <bmcg0011@student.monash.edu>
2024-08-01 16:11:31 +1000

    Make safety light file

---
+++ src/esda_safety_light.rs
@@ -0,0 +1,14 @@
+// use esp_hal::gpio::{Output, OutputPin};
+
+// pub const SAFETY_LIGHT_PIN_HANDLE: u8 = 10;
+// pub static SAFETY_LIGHT_HANDLE: Mutex<
+// RefCell<
+//     Option<Output<OutputPin<SAFETY_LIGHT_PIN_NUMBER>>>,
+// >,
+// > = Mutex::new(RefCell::new(None));
+
+// pub fn set_safety_light(state: bool) {
+//     critical_section::with(|cs| {
+        
+//     });
+// } 
